a1eedc56c1f58dc2f50eed1ba10571789e573239
jgutierrez-agssa <jgutierrez@agssa.net>
2025-05-19 20:27:58 +0000

    reparando for zeitwerk

--- app/decorators/controllers/refinery/fast_controller_decorator.rb
+++ app/decorators/controllers/refinery/fast_controller_decorator.rb
@@ -1,5 +1,15 @@
-Refinery::FastController.class_eval do
-  def wymiframe
-    render :template => "/refinery/wymiframe", :layout => false
+module Controllers
+  module Refinery
+    module FastControllerDecorator
+      def wymiframe
+        render template: "/refinery/wymiframe", layout: false
+      end
+    end
   end
 end
+
+# Precargar la clase original
+require_dependency "refinery/fast_controller"
+
+# Aplicar el decorador
+Refinery::FastController.prepend Controllers::Refinery::FastControllerDecorator
